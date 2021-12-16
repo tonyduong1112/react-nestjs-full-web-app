c01e6bc77473997b9c926aebfe7e95da80ae495f
DESKTOP-MQQ6FFB\LTC <tony.duong1112@gmail.com>
2021-12-16 16:10:07 +0700

    Fetching data from GraphQL API

--- react-cars-app/src/app/containers/HomePage/topCars.tsx
+++ react-cars-app/src/app/containers/HomePage/topCars.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { ICar } from "../../../typings/car";
@@ -7,6 +7,7 @@ import Carousel, { Dots, slidesToShowPlugin } from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
 import { useMediaQuery } from "react-responsive";
 import { SCREENS } from "../../components/responsive";
+import carService from "../../services/carService";
 
 const TopCarsContainer = styled.div`
   ${tw`
@@ -46,10 +47,17 @@ const CarsContainer = styled.div`
 `;
 
 export function TopCars() {
-
   const [current, setCurrent] = useState(0);
 
-  const isMobile = useMediaQuery({maxWidth: SCREENS.sm});
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  const fetchTopCars = async () => {
+    const cars = carService.getCars().catch((err) => {
+      console.log("ERROR: ", err);
+    });
+
+    console.log("Cars: ", cars);
+  };
 
   const testCar: ICar = {
     name: "Audi S3 Car",
@@ -73,7 +81,11 @@ export function TopCars() {
     gas: "Petrol",
   };
 
-  const cars =  [
+  useEffect(() => {
+    fetchTopCars();
+  }, []);
+
+  const cars = [
     <Car {...testCar2} />,
     <Car {...testCar} />,
     <Car {...testCar2} />,
@@ -83,45 +95,48 @@ export function TopCars() {
 
   const numberOfDots = isMobile ? cars.length : Math.ceil(cars.length / 3);
 
-  return <TopCarsContainer>
-    <Title>Explore Our Top Deals</Title>
-    <CarsContainer>
-      <Carousel value={current}
-        onChange={setCurrent}
-        slides={cars}
-        plugins={[
-          "clickToChange",
-          {
-            resolve: slidesToShowPlugin,
-            options: {
-              numberOfSlides: 3,
-            }
-          }
-        ]}
-        breakpoints={{
-          640: {
-            plugins: [
-              {
-                resolve: slidesToShowPlugin,
-                options: {
-                  numberOfSlides: 1,
-                },
+  return (
+    <TopCarsContainer>
+      <Title>Explore Our Top Deals</Title>
+      <CarsContainer>
+        <Carousel
+          value={current}
+          onChange={setCurrent}
+          slides={cars}
+          plugins={[
+            "clickToChange",
+            {
+              resolve: slidesToShowPlugin,
+              options: {
+                numberOfSlides: 3,
               },
-            ],
-          },
-          900: {
-            plugins: [
-              {
-                resolve: slidesToShowPlugin,
-                options: {
-                  numberOfSlides: 2,
+            },
+          ]}
+          breakpoints={{
+            640: {
+              plugins: [
+                {
+                  resolve: slidesToShowPlugin,
+                  options: {
+                    numberOfSlides: 1,
+                  },
                 },
-              },
-            ],
-          },
-        }}
-      />
-      <Dots value={current} onChange={setCurrent} number={numberOfDots} />
-    </CarsContainer>
-  </TopCarsContainer>
+              ],
+            },
+            900: {
+              plugins: [
+                {
+                  resolve: slidesToShowPlugin,
+                  options: {
+                    numberOfSlides: 2,
+                  },
+                },
+              ],
+            },
+          }}
+        />
+        <Dots value={current} onChange={setCurrent} number={numberOfDots} />
+      </CarsContainer>
+    </TopCarsContainer>
+  );
 }
