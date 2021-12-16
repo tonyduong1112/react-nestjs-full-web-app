6c5205d8c60b28f20543291e5e4be10c3ee08b65
DESKTOP-MQQ6FFB\LTC <tony.duong1112@gmail.com>
2021-12-16 13:03:07 +0700

    Create Car module and resolver

--- nestjs-cars-app/src/app.module.ts
+++ nestjs-cars-app/src/app.module.ts
@@ -1,12 +1,23 @@
+import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      playground: true,
+      debug: true,
+      autoSchemaFile: true
+    }),
+    ComponentsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ nestjs-cars-app/src/components/cars/cars.resolver.ts
@@ -0,0 +1,13 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { CarsService } from './cars.service';
+
+
+@Resolver()
+export class CarsResolver {
+  constructor(private carsService: CarsService) {}
+
+  @Query((returns) => String)
+  public async cars() {
+    return 'Hello';
+  }
+}
